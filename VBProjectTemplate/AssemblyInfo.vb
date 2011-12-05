b928dc832f5bb922705b8a7522c67b034c438375
piac <giulio@mcneel.com>
2011-12-05 22:28:12 +0100

    vb command support

--- VBProjectTemplate/AssemblyInfo.vb
+++ VBProjectTemplate/AssemblyInfo.vb
@@ -1,13 +1,23 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
+Imports Rhino.PlugIns
+
+' Plug-in Description Attributes - all of these are optional
+' These will show in Rhino's option dialog, in the tab Plug-ins
+<Assembly: PlugInDescription(DescriptionType.Address, "-")>
+<Assembly: PlugInDescription(DescriptionType.Country, "-")>
+<Assembly: PlugInDescription(DescriptionType.Email, "-")>
+<Assembly: PlugInDescription(DescriptionType.Phone, "-")>
+<Assembly: PlugInDescription(DescriptionType.Fax, "-")>
+<Assembly: PlugInDescription(DescriptionType.Organization, "-")>
+<Assembly: PlugInDescription(DescriptionType.UpdateUrl, "-")>
+<Assembly: PlugInDescription(DescriptionType.WebSite, "-")>
+
 
 ' General Information about an assembly is controlled through the following 
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
-
-' Review the values of the assembly attributes
-
 <Assembly: AssemblyTitle("$projectname$")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("$registeredorganization$")> 
@@ -18,7 +28,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("$guid1$")> 
+<Assembly: Guid("$guid1$")> ' This will also be the Guid of the Rhino plug-in
 
 ' Version information for an assembly consists of the following four values:
 '
