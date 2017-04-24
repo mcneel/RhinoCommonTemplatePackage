9d457f7dfdf7663b2565ad7acb58db8d03171f04
piac <giulio@mcneel.com>
2017-04-24 12:06:46 +0200

    Fix C# item bug

--- CSCommandItem/MyCommand.cs
+++ CSCommandItem/MyCommand.cs
@@ -1,11 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
-$endif$using System.Text;
+using Rhino;
+using Rhino.Commands;
 
 namespace $rootnamespace$
 {
-	class $safeitemrootname$
-	{
-	}
+    public class $safeitemrootname$ : Command
+    {
+        static $safeitemrootname$ _instance;
+        public $safeitemrootname$()
+        {
+            _instance = this;
+        }
+
+        ///<summary>The only instance of the $safeitemrootname$ command.</summary>
+        public static $safeitemrootname$ Instance
+        {
+            get { return _instance; }
+        }
+
+        public override string EnglishName
+        {
+            get { return "$safeitemrootname$"; }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // TODO: complete command.
+            return Result.Success;
+        }
+    }
 }
